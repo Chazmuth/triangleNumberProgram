cb2fb1a326efa1d44b0edf233015f8bc5cb23206
Justin Jobling <justinjobling@Justins-MacBook-Air.local>
2021-09-26 16:27:58 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int userInt = input.nextInt();
+        for (int i = 1; i <= userInt; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        for (int i = userInt-1; i > 0; --i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
